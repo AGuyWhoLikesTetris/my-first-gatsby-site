1358f6578fe850c1f3cd552efc9c4f6d26b994b5
Jaden Suh <jadenjsuh@gmail.com>
2023-07-20 19:36:05 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
